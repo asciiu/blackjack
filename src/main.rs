0efbe7f09c4b93f1049b1d8fb34880f4df93c803
Flowmaster <ellyssin.gimhae@gmail.com>
2019-02-06 19:12:05 -0700

    Start of shoe struct.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use std::io;
 
+const CARDS: [char; 14] = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', '1'];
+const SUIT: [char; 4] = ['C', 'D', 'H', 'S'];
+
+#[derive(Debug)]
+struct Card {
+    value: char,
+    suit: char,
+}
+
+#[derive(Debug)]
+struct Shoe {
+     cards: Vec<Card>
+}
+
+impl Shoe {
+    fn new() -> Shoe {
+        let mut shoe = Shoe{ cards: Vec::new() };
+        for c in CARDS.iter() {
+            for s in SUIT.iter() {
+                let card = Card{ value: *c, suit: *s };
+                shoe.cards.push(card);
+            }
+        }
+        shoe
+    }
+}
 
 fn play_again() -> bool {
     println!("Play again? [y|n]");
@@ -8,10 +34,8 @@ fn play_again() -> bool {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    // trim newline from input
-    input = String::from(input.trim());
 
-    match input.as_ref() {
+    match input.trim() {
         "y" => true,
         _ => false
     }
@@ -36,6 +60,9 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue
         };
+
+        let shoe = Shoe::new();
+        println!("{:#?}", shoe);
 
         println!("balance: {}", balance);
 

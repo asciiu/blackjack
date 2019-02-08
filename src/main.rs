ffe3c669379c5a189c2e6b61ea586803059604a6
Flowmaster <ellyssin.gimhae@gmail.com>
2019-02-07 20:40:44 -0700

    Refactor of read int.

--- src/main.rs
+++ src/main.rs
@@ -18,26 +18,42 @@ fn play_again() -> bool {
     }
 }
 
+fn read_int() -> i32 {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // trim trailing new line
+    input = String::from(input.trim());
+
+    match input.parse() {
+        Ok(num) => num,
+        Err(_) => -1 
+    }
+}
+
 fn main() {
     let mut player_continue: bool = true;
     let mut shoe = Shoe::new();
+    let mut balance: i32;
 
-    while player_continue {
+    while player_continue  {
         println!("Enter a bankroll Grin amount (e.g. 1000)");
+        balance = read_int();
 
-        let mut roll = String::new();
+        if balance < 0 {
+            continue
+        }
 
-        io::stdin()
-            .read_line(&mut roll)
-            .expect("Failed to read line");
+        let mut wagered_amount: i32 = 0;
+        while wagered_amount == 0 {
+            println!("Enter wager amount (balance {})", balance);
+            wagered_amount = read_int();
+        }
 
-        // trim trailing new line
-        roll = String::from(roll.trim());
-
-        let balance: i32 = match roll.parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
+        println!("Wager amount: {}", wagered_amount);
 
         shoe.shuffle();
         match shoe.deal_card() {
@@ -45,6 +61,7 @@ fn main() {
             _ => shoe = Shoe::new(),
         }
 
+        balance -= wagered_amount;
         println!("balance: {}", balance);
 
         player_continue = play_again();

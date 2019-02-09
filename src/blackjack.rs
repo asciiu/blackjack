0bb41618548e9ca98e28a33338192c381854488d
Flowmaster <ellyssin.gimhae@gmail.com>
2019-02-09 00:09:22 -0700

    Blackjack run should not loop.

--- src/blackjack.rs
+++ src/blackjack.rs
@@ -49,7 +49,7 @@ impl Player {
     }
 
     fn print_hand(&mut self, label: &str) {
-        print!("\n{}: ", label);
+        print!("{}: ", label);
         for c in self.cards.iter() {
             print!("{} ", c);
         }
@@ -65,6 +65,7 @@ fn read_input() -> PlayerInput {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
+    println!("");
 
     // trim trailing new line
     input = String::from(input.trim());
@@ -108,55 +109,61 @@ impl Blackjack {
     }
 
     pub fn run(&mut self) {
-        loop {
+        let player_card: Card = self.deal_card(); 
+        let dealer_card: Card = self.deal_card(); 
+        println!("Dealer: {}", dealer_card);
+        println!("Player: {}", player_card);
 
-            let player_card: Card = self.deal_card(); 
-            let dealer_card: Card = self.deal_card(); 
-            println!("Dealer: {}", dealer_card);
-            println!("Player: {}", player_card);
+        self.dealer.cards.push(dealer_card);
+        self.player.cards.push(player_card);
 
-            self.dealer.cards.push(dealer_card);
-            self.player.cards.push(player_card);
+        let mut player_score = self.player.score();
+        let mut dealer_score = self.dealer.score();
+        let mut stand = false;
 
-            let mut player_score = self.player.score();
-            let mut dealer_score = self.dealer.score();
-            let mut stand = false;
-
-            while player_score < 21 && !stand {
-                let input = read_input();
-                match input {
-                    PlayerInput::Hit => {
-                        let player_card: Card = self.deal_card(); 
-                        self.player.cards.push(player_card);
-                        self.player.print_hand("Player");
-                        player_score = self.player.score();
-                    }
-
-                    PlayerInput::Stand => stand = true,
-
-                    _ => ()
+        while player_score < 21 && !stand {
+            let input = read_input();
+            match input {
+                PlayerInput::Hit => {
+                    let player_card: Card = self.deal_card(); 
+                    self.player.cards.push(player_card);
+                    self.player.print_hand("Player");
+                    player_score = self.player.score();
                 }
-            }
 
-            while dealer_score < 17 {
-                let dealer_card: Card = self.deal_card(); 
-                self.dealer.cards.push(dealer_card);
-                dealer_score = self.dealer.score();
-            }
-            self.dealer.print_hand("Dealer");
-            //println!("Dealer score: {}", dealer_score);
-
-            //if player_score > 21 {
-            //    println!("Player score: {} (bust)", player_score);
-            //} else {
-            //    println!("Player score: {}", player_score);
-            //}
-            
-            if player_score > 21 || dealer_score > 21 {
-                break
+                PlayerInput::Stand => {
+                    stand = true;
+                    self.player.print_hand("Player");
+                }
+                _ => ()
             }
         }
 
+        while dealer_score < 17 {
+            let dealer_card: Card = self.deal_card(); 
+            self.dealer.cards.push(dealer_card);
+            dealer_score = self.dealer.score();
+        }
+        
+        self.dealer.print_hand("Dealer");
+
+        //println!("Dealer score: {}", dealer_score);
+
+        if player_score > dealer_score && player_score <= 21 {
+            println!("Player (win!)");
+        } else if player_score < dealer_score && dealer_score <= 21 {
+            println!("Player (lose)");
+        } else if player_score > 21 && dealer_score <= 21 {
+            println!("Player (bust)");
+        } else if dealer_score > 21 && player_score <= 21 {
+            println!("Dealer (bust)");
+        } else if dealer_score == player_score {
+            println!("push");
+        } else if dealer_score > 21 && player_score > 21 {
+            println!("push");
+        }
+        println!("");
+            
         self.player.cards.clear();
         self.dealer.cards.clear();
     }
